2c8f9a61f46e1a92fe5c4ac44faf6bb471188343
Hugo Ljungqvist <hulj07001@utb.vaxjo.se>
2024-12-18 11:54:31 +0100

    Add project files.

---
+++ inlämning4_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.SymbolStore;
+using System.Linq.Expressions;
+namespace inlämning4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int tal;
+                int störst = 0;
+
+
+                string svar = null;
+                do
+                {
+                    Console.WriteLine("Skriv in ett heltal");
+                    tal = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Vill du fortsätta? j/n.");
+                    svar = Console.ReadLine().ToLower();
+
+                    if (tal >= störst)
+                    {
+                        störst = tal;
+                    }
+
+                } while (svar == "j");
+                {
+                    Console.WriteLine($"Det största talet du gav var: {störst}.");
+                    Console.WriteLine("Avslutar programmet");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("OBS! Fel vid inmatning.");
+            }
+        }   
+    }
+}
